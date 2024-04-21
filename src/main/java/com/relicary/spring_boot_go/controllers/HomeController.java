5a25ff5c1f8352502d1519ad60982b19faa0efdc
Juan Cremades <relicary@gmail.com>
2024-04-21 18:31:05 +0200

    Added a basic MVC in a GET request

--- src/main/java/com/relicary/spring_boot_go/controllers/HomeController.java
+++ src/main/java/com/relicary/spring_boot_go/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.relicary.spring_boot_go.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String displayHomePage() {
+    @GetMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+    model.addAttribute("username", "Vincent D'Agosta");
         return "home.html";
     }
 
