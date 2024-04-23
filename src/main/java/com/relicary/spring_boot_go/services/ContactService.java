9ee9953a772934e136e233b3608d3f0d0c261795
Juan Cremades <relicary@gmail.com>
2024-04-23 17:55:00 +0200

    REQUEST SCOPE example
    Refs #7

--- src/main/java/com/relicary/spring_boot_go/services/ContactService.java
+++ src/main/java/com/relicary/spring_boot_go/services/ContactService.java
@@ -1,12 +1,25 @@
 package com.relicary.spring_boot_go.services;
 
 import com.relicary.spring_boot_go.model.Contact;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Service
 @Log4j2
+@Getter
+@Setter
+@RequestScope
 public class ContactService {
+    
+    private int counter = 0;
+
+    public ContactService(){
+        log.info("Contact Service Bean initialized");
+    }
+    
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
         // TODO: Persist in a DB Table
